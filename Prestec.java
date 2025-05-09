a4f884a093bd94447d20f63665161c120118c0d3
Youssef Ghaddari <youssef.ghaddari@insbaixcamp.cat>
2025-05-09 20:36:04 +0200

    Classe Prestec acabada

--- Prestec.java
+++ Prestec.java
@@ -3,22 +3,38 @@ package gestioBiblioteca2;
 import java.time.LocalDate;
 
 public class Prestec {
-	
-	private Usuari usuari;
-	private Llibre llibre;
-	private LocalDate dataPrestec;
-	private LocalDate dataRetorn;
-	
-	public Prestec(Usuari usuari, Llibre llibre, LocalDate dataPrestec) {
-		this.usuari = usuari;
-		this.llibre = llibre;
-		this.dataPrestec = dataPrestec;
-		this.dataRetorn = dataPrestec.plusWeeks(2);
-	}
-	
-	public Usuari getUsuari() { return usuari; }
-	
-	public Llibre getLlibre() { return llibre; }
-	
-	public LocalDate getDataRetorn() { return dataRetorn; }
+    private Usuari usuari;
+    private Llibre llibre;
+    private LocalDate dataPrestec;
+    private LocalDate dataRetorn;
+    
+    public Prestec(Usuari usuari, Llibre llibre, LocalDate dataPrestec) {
+        this.usuari = usuari;
+        this.llibre = llibre;
+        this.dataPrestec = dataPrestec;
+        // Data de retorn esperada: 2 setmanes després
+        this.dataRetorn = dataPrestec.plusWeeks(2);
+    }
+    
+    public Usuari getUsuari() { 
+        return usuari; 
+    }
+    
+    public Llibre getLlibre() { 
+        return llibre; 
+    }
+    
+    public LocalDate getDataPrestec() { 
+        return dataPrestec; 
+    }
+    
+    public LocalDate getDataRetorn() { 
+        return dataRetorn; 
+    }
+    
+    @Override
+    public String toString() {
+        return "Prestec: " + llibre.getTitol() + " prestat a " + usuari.getNom() + 
+               " el " + dataPrestec + ", a retornar abans de " + dataRetorn;
+    }
 }
