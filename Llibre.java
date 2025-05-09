149365eda8f99e86c14f841b5525e24b65911b01
Carles Lara <carles.lara@insbaixcamp.cat>
2025-05-09 17:46:59 +0200

    classe llibre actualitzat

--- Llibre.java
+++ Llibre.java
@@ -1,28 +1,49 @@
-package gestioBiblioteca2;
+package gestioBiblioteca;
 
-public class Llibre {	
-	private String titol;
-	private String autor;
-	private boolean prestat;
-	
-	public Llibre(String titol, String autor) {
-		this.titol = titol;
-		this.autor = autor;
-		this.prestat = false;
-	}
-	
-	public String getTitol() { return titol; }
-	
-	public String getAutor() { return autor; }
-	
-	public boolean esPrestat() { return prestat; }
-	
-	public void prestar() { prestat = true; }
-	
-	public void retornar() { prestat = false; }
-	
-	@Override
-	public String toString() {
-		return titol + " de " + autor + (prestat ? " (En préstec)" : " (Disponible)");
-	}
+public class Llibre {
+    private String titol;
+    private String autor;
+    private boolean prestat;
+    private String categoria;
+    
+    public Llibre(String titol, String autor, String categoria) {
+        this.titol = titol;
+        this.autor = autor;
+        this.categoria = categoria;
+        this.prestat = false;
+    }
+    
+    public String getTitol() { 
+        return titol; 
+    }
+    
+    public String getAutor() { 
+        return autor; 
+    }
+    
+    public boolean esPrestat() { 
+        return prestat; 
+    }
+    
+    public String getCategoria() {
+        return categoria;
+    }
+    
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    public void prestar() { 
+        prestat = true; 
+    }
+    
+    public void retornar() { 
+        prestat = false; 
+    }
+    
+    @Override
+    public String toString() {
+        return titol + " de " + autor + " (Categoria: " + categoria + ") " + 
+               (prestat ? "(En préstec)" : "(Disponible)");
+    }
 }
